51638eab210e6953a9c342db08178f32c74ef645
GilbertoRdzP <A01635693@itesm.mx>
2021-08-21 02:15:14 +0000

    .

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    b = int(input("Dame la base: "))
+    h = int(input("Dame la altura: "))
+
+    a = (b * h)/2
+
+    print(f"El área es: {a}")
+
+
+
 
 
 
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,18 @@
 def main():
     #escribe tu código abajo de esta línea
+    import math
+    promedio = []
+
+    promedio.append(int(input("Calificación de la materia: ")))
+    promedio.append(int(input("Calificación de la materia: ")))
+    promedio.append(int(input("Calificación de la materia: ")))
+    promedio.append(int(input("Calificación de la materia: ")))
+
+    average = math.fsum(promedio)/len(promedio)
+
+    print(f"El promedio es: {average}")
+
+
 
 
 if __name__ == '__main__':
